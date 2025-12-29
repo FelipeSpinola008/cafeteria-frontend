f7fc87424ae44556255edcfa3171a5fefd114593
FelipeSpinola008 <felipespinola784@gmail.com>
2025-12-29 17:41:34 -0300

    :rocket: feat: implement header component with custom typography and layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import { Header } from "./components/Header"
 function App() {
   return (
     <div>
-      <header ><Header/></header>
-      <main className="p-8">
-        <h2 className="text-xl text-amber-800">Bem-vindo ao Menu</h2>
-        <p className="mt-4">Aqui você poderá gerenciar seus cafés.</p>
-      </main>
+    
+    <header ><Header/></header>
+    <main></main>
+    
+      
     </div>
       
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 export function Header() {
   return (
-    <header className="bg-black">
-      <h1 className="text-2xl font-bold">Cafeteria Dev</h1>
-        <p>Olá mundo</p>
+    <header className="flex items-center justify-between bg-transparent pl-16 pr-16 pt-10 ">
+      <div className="flex justify-between font-playfair text-amber-950 text-xl font-bold">
+        <a href="">Cafeteria-dev</a>
+      </div>
+      <nav className="flex gap-8 font-poppins text-amber-950 text-sm font-extralight ">
+        <a href="">HOME</a>
+        <a href="">CAFÉS</a>
+        <a href="">PADARIA</a>
+        <a href="">CARRINHO</a>
+        <a href="">SOBRE</a>
+        <a href="">LOGIN</a>
+      </nav>
+    
+        
     </header>
   );
 }
