b1575d62f85986351cc8763aa371b794e7de9812
FelipeSpinola008 <felipespinola784@gmail.com>
2025-12-30 00:50:30 -0300

    :rocket: feat: complete hero section with typography and call to action

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
 import coffeBg from '../assets/coffeBg.png';
-import { ShoppingCart, User } from 'lucide-react';
+
 
 export function Hero() {
     return (
-        <section className='relative h-screen w-full overflow-hidden bg-stone-950'>
+        <section className='relative h-screen w-full overflow-hidden bg-stone-950 flex items-center px-16'>
             <div className='absolute inset-0 z-0'>
                 <img 
                 src={coffeBg}
@@ -12,10 +12,24 @@ export function Hero() {
                 />
                 <div className='absolute inset-0 bg-gradient-to-r from-stone-950 via-stone-950/60 to-transparent' />
             </div>
-            <div>
-                <h2>
-                    We serve the best coff of the city
-                </h2>
+            <div className='relative z-10 max-2-3xl'>
+                <span className='font-poppins text-sm tracking-[0.2em] text-white-coffee'>
+                    Seja bem vindo!
+                </span>
+                <h1 className='font-playfair text-7xl md:text-8xl text-white leading-[1.1] mb-8'>
+                    Nós temos o melhor <br />
+                    <span className='italic font-light'>café da cidade </span>
+                </h1>
+                <p className='font-poppins text-lg text-stone-300 mb-10 max-w-lg leading-relaxed'>
+                    Experimente a riqueza de grãos cuidadosamente selecionados e a torra perfeita em cada xícara.
+                </p>
+
+                <div>
+                    <button className='bg-white-coffee text-stone-950 px-10 py-4 rounded-full font-poppins font-bold hover:bg-white transition-all shadow-lg transform hover:scale-105'>
+                        PEÇA AGORA
+                    </button>
+                    <button></button>
+                </div>
             </div>
         </section>
     )
